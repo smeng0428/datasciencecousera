d7562004f1668517cf34f4a85d44b2c266493fb6
Shawn Meng <67433641+smeng0428@users.noreply.github.com>
2021-03-04 18:23:18 -0500

    Add files via upload

---
+++ Exploratory Data Analysis/Exploratory Data Analysis Assignment #1.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(lubridate)
+##readin data
+unzip("exdata_data_household_power_consumption.zip")
+power <- read.csv2("household_power_consumption.txt", header = TRUE)
+power <- as.tibble(power)
+
+#filter for data from 2007-02-01 and 2007-02-02
+power1 <- filter(power, Date=="1/2/2007"|Date=="2/2/2007")
+##change data type from char to appropriate forms
+power1[,3:9] <- sapply(power1[,3:9], as.numeric)
+power1[,1] <- lapply(power1[,1], dmy)
+power1[,2] <- lapply(power1[,2], hms)
+
